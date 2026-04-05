f931fb85afd3c2b831024c1554c33f25c0bd873b
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2026-04-04 20:20:45 -0600

    Fixing the app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const router = express.Router();
 const cors = require("cors");
 
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const ticketRoutes = require("./routes/ticketRoutes");
+const { protect } = require("../middleware/authMiddleware");
 
 app.use(cors());
 app.use(express.json());
 // Prefijo para todas las rutas de productos
+router.post("/", protect, ticketController.createTicket);
 app.use("/api/products", productRoutes);
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/auth", authRoutes);
